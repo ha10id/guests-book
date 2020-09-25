1f95ba4dfdf04a7ee9091e84223692f544cbe6c8
ha10id <achiduzu@gmail.com>
2020-09-25 16:14:56 +0300

    add handlebars template

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,83 @@
-const express = require('express');
-const app = express();
+var _ = require("lodash");
+const path = require('path');
+const express = require("express");
+const exphbs = require('express-handlebars');
+const bodyParser = require("body-parser");
+const low = require("lowdb");
+const FileAsync = require("lowdb/adapters/FileAsync");
+
 const port = 3000;
+const app = express();
+const adapter = new FileAsync("DB/db.json");
 
-app.get('/', (request, response) => {
-    console.log(request.url);
-    response.end('Hello Express Server!');
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(bodyParser.json());
+app.use((err, request, response, next) => {
+  // логирование ошибки, пока просто console.log
+  console.log(err);
+  response.status(500).send("Something broke!");
+  next();
 });
 
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err);
-    }    
-  console.log(`server is listening on ${port}`);
-});
+low(adapter)
+  .then((db) => {
+    // Routes
+    app.get("/", (request, response) => {
+      let post = db
+      .get("posts")
+      .value();
+      response.render("home", {
+        post: post,
+      });
+    });
+    // GET /posts/
+
+    app.get("/posts", (req, res) => {
+      let post;
+      if (!_.isEmpty(req.query)) {
+        const { page } = req.query;
+        const recPerPage = 3;
+        if (page) {
+          let pageInt = parseInt(page) === 0 ? 1 : parseInt(page);
+          let startRec = pageInt === 1 ? 0 : (pageInt - 1) * recPerPage;
+          if (pageInt) {
+            // console.log(pageInt);
+            post = db
+              .get("posts")
+              .slice(startRec, startRec + recPerPage)
+              .value();
+          }
+        }
+      } else {
+        post = db.get("posts").slice(3, 6).value();
+      }
+      //console.log(JSON.stringify(post,null,'\t'));
+      res.send(post);
+    });
+    // GET /posts/:id
+    app.get("/posts/:id", (req, res) => {
+      const post = db.get("posts").find({ id: req.params.id }).value();
+      res.send(post);
+    });
+    // POST /posts
+    app.post("/posts", (req, res) => {
+      db.get("posts")
+        .push(req.body)
+        .last()
+        .assign({ id: Date.now().toString() })
+        .write()
+        .then((post) => res.send(post));
+    });
+
+    // Set db default values
+    return db.defaults({ posts: [] }).write();
+  })
+  .then(() => {
+    app.listen(port, () => console.log("listening on port 3000"));
+  });
